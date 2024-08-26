105293806c9b697d59d5e66d7a968eb6c5825610
g-xxoxx <trungduc05112000@gmail.com>
2024-08-26 16:12:17 +0700

    handleError

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 require('dotenv').config()
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
 
 
 const app = express()
 const port = process.env.PORT || 8888
+dbConnect();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use("/", (req, res) => { res.send('Server ONN') })
-
+initRoutes(app)
 
 app.listen(port, () => {
     console.log("Server running on the port:", port)
 })
+
